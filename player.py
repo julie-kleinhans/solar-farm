e10599b8ce35983a7abffa8b46f0b7af6f0ad352
Julie Kleinhans <julie.kleinhans@outlook.fr>
2020-05-28 08:50:36 +0200

    Changement de lien

--- player.py
+++ player.py
@@ -13,19 +13,77 @@ class Player:
         self.battery_stock = np.zeros(49) #a(t)
         self.capacity = 100
         self.max_load = 70
-        self.prices = {"internal" : [],"external_purchase" : [],"external_sale" : []}
-        self.imbalance=[]
+        self.prices = {"purchase" : [],"sale" : []}
+        self.imbalance={"purchase_cover":[], "sale_cover": []}
+        self.memoire_NRJ = 0
+        self.memoire_prix_interne = np.zeros(48)
 
     def take_decision(self, time):
+        #ON A AJOUTE self.memoire_NRJ = 0 
+        # et self.memoire_prix_interne = np.zeros(48)
+        #DANS _INIT_. 
+        #FAITES ATTENTION SI VOUS COPIEZ DES PARTIES DU CODE, MERCI !
+        
+        duree_pas_de_temps = self.dt
+        chargement_batterie = 0
+        
+        moyenne_prix_journee = 0
+        for temps in range (12,37):
+            moyenne_prix_journee += self.memoire_prix_interne[temps]
+        moyenne_prix_journee = moyenne_prix_journee/24 
+        
+        if (time == 40):
+            self.memoire_NRJ = self.battery_stock[39] / 8
 
-            # TO DO:
-            # implement your policy here to return the load charged / discharged in the battery
-            # below is a simple example
+        NRJ = self.memoire_NRJ 
+        if (NRJ > self.max_load * duree_pas_de_temps):
+            cas = 1  # batterie chargee a bloc
+            NRJ_restante = 8 *(NRJ - self.max_load * duree_pas_de_temps)  # pour decharger la nuit
+        else:
+            cas = 2  # batterie pas chargee a fond
+
+        if time >= 20 and time < 36:  # chargement de la batterie au milieu de la journee
+            if (self.memoire_prix_interne[time] < moyenne_prix_journee):
+                chargement_batterie = (self.sun[time - 1] / 2) + 1  # +1 au cas ou sun marche pas
+            else : 
+                chargement_batterie = 0
             
-            if time>20 and time<35:
-                return -8
+            if (self.battery_stock[
+                    time - 1] + chargement_batterie * duree_pas_de_temps) > self.capacity:  # verification de la capacite
+                chargement_batterie = (self.capacity - self.battery_stock[time - 1]) / duree_pas_de_temps
+
+        elif (time >= 12) and (time <= 15):  # dechargement le matin (heure de pointe)
+            if (cas == 2):
+                chargement_batterie = - NRJ / duree_pas_de_temps
             else:
-                return +5
+                chargement_batterie = - self.max_load
+
+        elif ((time >= 40) and (time <= 43)):  # dechargement le soir (heure de pointe)
+            if (cas == 2):
+                chargement_batterie = - NRJ / duree_pas_de_temps
+            else:
+                chargement_batterie = - self.max_load
+
+        elif ((time < 12) or (43 < time)):  # dechargement du reste de la batterie la nuit
+            if (cas == 1):
+                chargement_batterie = - NRJ_restante / duree_pas_de_temps / (12 + 5)
+                # 5 correspond au nombre de pas de temps entre 22H et minuit ???
+            else:
+                chargement_batterie = 0
+        
+        # Enregistrement du prix 
+        if (time == 0):
+            self.memoire_prix_interne[47] = 0 
+            # on aimerait bien self.prices["sale"][47] mais au premier jour de la simulation ça ne passe pas
+            # Solution possible : faire un compteur pour les jours 
+            # Mais flemme parce que de toute façon on ne se sert pas du prix à minuit 
+        else:
+            self.memoire_prix_interne[time-1] = self.prices["sale"][time-1]
+        
+        # On vérifie qu'on ne dépasse pas la puissance max.
+        if (abs(chargement_batterie) > self.max_load):
+            chargement_batterie = self.max_load * np.sign(chargement_batterie)
+        return chargement_batterie
 
     def update_battery_stock(self, time,load):
             if abs(load) > self.max_load:
@@ -58,12 +116,11 @@ class Player:
     def observe(self, t, sun, price, imbalance):
         self.sun.append(sun)
         
-        self.prices["internal"].append(price["internal"])
-        self.prices["external_sale"].append(price["external_sale"])
-        self.prices["external_purchase"].append(price["external_purchase"])
-        
-        self.imbalance.append(imbalance)
-        
+        self.prices["purchase"].append(price["purchase"])
+        self.prices["sale"].append(price["sale"])
+
+        self.imbalance["purchase_cover"].append(imbalance["purchase_cover"])
+        self.imbalance["sale_cover"].append(imbalance["sale_cover"])
     
     def reset(self):
         self.load= np.zeros(48)
@@ -74,5 +131,5 @@ class Player:
         self.battery_stock[0] = last_bat
         
         self.sun=[]
-        self.prices = {"internal" : [],"external_purchase" : [],"external_sale" : []}
-        self.imbalance=[]
+        self.prices = {"purchase" : [],"sale" : []}
+        self.imbalance={"purchase_cover":[], "sale_cover": []}
